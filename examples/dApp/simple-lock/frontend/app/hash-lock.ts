dc61de4844aee0c3203d7c6d7e73c2a49f57dcf5
Your GitHub Username <daodudestiny56@gmail.com>
2026-07-31 00:04:45 +0100

    fix: replace manual fee calculation in hash-lock.ts with tx.completeFeeBy(readSigner)

--- examples/dApp/simple-lock/frontend/app/hash-lock.ts
+++ examples/dApp/simple-lock/frontend/app/hash-lock.ts
@@ -92,15 +92,7 @@ export async function unlock(
     readSigner,
     ccc.fixedPointFrom(occupiedSize),
   );
-  const balanceDiff =
-    (await tx.getInputsCapacity(cccClient)) - tx.getOutputsCapacity();
-  console.log("balanceDiff: ", balanceDiff);
-  if (balanceDiff > ccc.Zero) {
-    tx.addOutput({
-      lock: fromScript,
-      capacity: balanceDiff - BigInt(1000), // Fee 1000
-    });
-  }
+  await tx.completeFeeBy(readSigner);
 
   // fill the witness with preimage
   const preimageAnswer = window.prompt("please enter the preimage: ");
